5557da1851649fe45434e9f1afd605279ee17e10
Ederson Berti <edersonmberti@gmail.com>
2021-06-24 22:04:20 -0300

    feat: add tests

--- Memorize/MemorizeTests/MemorizeTests.swift
+++ Memorize/MemorizeTests/MemorizeTests.swift
@@ -10,6 +10,12 @@ import XCTest
 
 class MemorizeTests: XCTestCase {
 
+    var memoryGame: MemoryGame<String>!
+    
+    override func setUp() {
+        memoryGame = MemoryGame(of: ["A"])
+    }
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -29,5 +35,67 @@ class MemorizeTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+    
+    func test_shouldGameFinishWhenMatchedAllCards() {
+        memoryGame.chooseCard(at: 0)
+        memoryGame.chooseCard(at: 1)
+        
+        XCTAssertTrue(memoryGame.cards[0].isFaceUp)
+        XCTAssertTrue(memoryGame.cards[1].isFaceUp)
+        XCTAssertTrue(memoryGame.finish)
+    }
+    
+    func test_shouldKeepChosenCardFaceUpIfCardAlreadyFaceUp() {
+        memoryGame.chooseCard(at: 0)
+        
+        XCTAssertTrue(memoryGame.cards[0].isFaceUp)
+        
+        memoryGame.chooseCard(at: 0)
+        XCTAssertTrue(memoryGame.cards[0].isFaceUp)
+    }
+    
+    func test_shouldCreateCorrectPairsWhenInit() {
+        XCTAssertEqual(memoryGame.cards.count, 2)
+        
+        memoryGame = MemoryGame(of: ["a", "b"])
+        XCTAssertEqual(memoryGame.cards.count, 4)
+        
+        memoryGame = MemoryGame(of: ["a", "b", "c"])
+        XCTAssertEqual(memoryGame.cards.count, 6)
 
+        memoryGame = MemoryGame(of: ["a", "b", "c", "d"])
+        XCTAssertEqual(memoryGame.cards.count, 8)
+        
+        memoryGame = MemoryGame(of: ["a", "b", "c", "d", "e"])
+        XCTAssertEqual(memoryGame.cards.count, 10)
+    }
+    
+    func test_shouldIncreaseAttemptsWhenChooseTwoCards() {
+        memoryGame.chooseCard(at: 0)
+        memoryGame.chooseCard(at: 1)
+        
+        XCTAssertEqual(memoryGame.attempts, 1)
+    }
+    
+    func test_shouldKeepJustOneCardFaceUpWhenChooseThreeCards() {
+        memoryGame = MemoryGame(of: ["a", "b"])
+        
+        memoryGame.chooseCard(at: 0)
+        memoryGame.chooseCard(at: 1)
+        memoryGame.chooseCard(at: 2)
+        
+        XCTAssertFalse(memoryGame.cards[0].isFaceUp)
+        XCTAssertFalse(memoryGame.cards[1].isFaceUp)
+        XCTAssertTrue(memoryGame.cards[2].isFaceUp)
+    }
+    
+    func test_shouldDoNothingWhenChooseAnInvalidIndex() {
+        memoryGame.chooseCard(at: 10)
+        memoryGame.chooseCard(at: 11)
+        memoryGame.chooseCard(at: 12)
+        memoryGame.chooseCard(at: 13)
+        
+        XCTAssertFalse(memoryGame.cards[0].isFaceUp)
+        XCTAssertFalse(memoryGame.cards[1].isFaceUp)
+    }
 }
